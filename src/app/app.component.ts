b8f324a060e72be957a87e564f7292ce82cf2a0b
mbie95 <marcinbielen995@gmail.com>
2023-02-24 23:39:46 +0100

    Add files

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = "Marcin Bielen IT";
+  footer = `2021 © ${this.title} , All rights reserved.`;
+  aboutMe = "sduaihdiashduihasuihdiuashduiahsuidhauihdiuhaiudhasuidhuiashduihuidisduaihdiashduihasuihdiuashduiahsuidhauihdiuhaiudhasuidhuiashduihuidisduaihdiashduihasuihdiuashduiahsuidhauihdiuhaiudhasuidhuiashduihuidisduaihdiashduihasuihdiuashduiahsuidhauihdiuhaiudhasuidhuiashduihuidisduaihdiashduihasuihdiuashduiahsuidhauihdiuhaiudhasuidhuiashduihuidisduaihdiashduihasuihdiuashduiahsuidhauihdiuhaiudhasuidhuiashduihuidisduaihdiashduihasuihdiuashduiahsuidhauihdiuhaiudhasuidhuiashduihuidisduaihdiashduihasuihdiuashduiahsuidhauihdiuhaiudhasuidhuiashduihuidisduaihdiashduihasuihdiuashduiahsuidhauihdiuhaiudhasuidhuiashduihuidi";
+}
